9f7ad020a41af1e66dd9cc1ad7c088dbe018e6c6
LilyisusUSACH <alejandro.cortes.c@usach.cl>
2024-05-17 01:58:36 -0400

    [Alejandro Cortes] Conexion entre microservicios establecida

--- micro1/src/main/java/ajcc/autofix/micro1/Controllers/TestController.java
+++ micro1/src/main/java/ajcc/autofix/micro1/Controllers/TestController.java
@@ -1,16 +1,22 @@
 package ajcc.autofix.micro1.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @CrossOrigin("*")
 public class TestController {
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @GetMapping("/hello")
     public ResponseEntity<?> a(){
-        return ResponseEntity.ok("Correcto");
+        String a = restTemplate.getForObject("http://micro2/",String.class);
+        return ResponseEntity.ok("Traido desde narnia" + a);
     }
 }
--- micro2/src/main/java/ajcc/autofix/micro2/Micro2Application.java
+++ micro2/src/main/java/ajcc/autofix/micro2/Micro2Application.java
@@ -1,8 +1,10 @@
 package ajcc.autofix.micro2;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,5 +13,4 @@ public class Micro2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Micro2Application.class, args);
 	}
-
 }
