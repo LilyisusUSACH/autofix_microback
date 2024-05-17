b036a8bfd9b4d1c8ead9949026c2ce019523d3d2
LilyisusUSACH <alejandro.cortes.c@usach.cl>
2024-05-16 23:58:30 -0400

    [Alejandro Cortes] Creacion de proyectos

---
+++ micro3/src/test/java/ajcc/autofix/micro3/Micro3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ajcc.autofix.micro3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Micro3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
